f575762cdce84a8b0495b5e1df45b53416921588
Kun <kun.register@gmail.com>
2020-04-01 05:10:05 +0800

    Create comment feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+    def create
+        # get article. and spawn comment by article. save comment. go back to show article page.
+        # @article = Article.find(params[])
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(params.require(:comment).permit(:commenter, :body))
+        @comment.save
+        redirect_to @article
+    end
 end
