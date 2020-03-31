97ea6a20cf4b281dd17573311cd7ad6daebc71f6
Kun <kun.register@gmail.com>
2020-03-31 13:25:40 +0800

    implement article update feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,10 +9,24 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(permit_params)
+            redirect_to @article  # this will hit GET /articles/id 
+        else
+            render 'edit'
+        end
+    end
+
     def create
         @article = Article.new(permit_params)
         if @article.save
-            redirect_to @article  # this will hit /articles url
+            redirect_to @article  # this will hit GET /articles/id 
         else
             render 'new'
         end
